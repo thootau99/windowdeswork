7a162eff93f41a0dcdcc517eec3096358ddd2ef2
蔡宇翔 <tasiyusiang@caiyuxiangdeAir.mshome.net>
2021-05-16 19:55:20 +0800

    new

--- front/src/main/java/ClientUI.java
+++ front/src/main/java/ClientUI.java
@@ -8,29 +8,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ClientUI {
-    String NAME = "Mark"; // The name of user
+    String NAME = "Default"; //使用者預設名稱
     String allMessage = "";
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
-    JPanel messagePanel = new JPanel(); //
-    JTextPane messages = new JTextPane(); // All messages go to here
-    JTextField inputMessage = new JTextField(); // The place that uses to type message
-    JButton sendMessage = new JButton("Send Message"); // The button that sends message
+    JPanel messagePanel = new JPanel();
+    JTextPane messages = new JTextPane(); //聊天室訊息
+    JTextField inputMessage = new JTextField(); //使用者輸入文字
+    JButton sendMessage = new JButton("Send Message"); //傳送輸入文字給聊天室
     JMenuBar menu = new JMenuBar();
     JMenu nameMenu = new JMenu("name");
-    JMenuItem setName = new JMenuItem("setName"); // The menu button that open a dialog to set name
+    JMenuItem setName = new JMenuItem("setName"); // 開始對話窗來更改使用者名字
 
     public void setFromServer (JSONObject message) throws JSONException {
         String combine = String.format("%s %s : %s \n", message.get("time"), message.get("username"), message.get("message"));
-        allMessage += combine;
-        messages.setText(allMessage);
-        frame.repaint();
+        allMessage += combine; //加入新字串（combine）更新現有字串（allMessage）
+        messages.setText(allMessage);//更新全部訊息字串
+        frame.repaint();//更新聊天視窗視窗內容
     }
 
     public void setInit() {
         menu.add(nameMenu);
         nameMenu.add(setName);
-        setName.addActionListener(new ActionListener() {
+        setName.addActionListener(new ActionListener() { //檢查使用者使否有要更改名字
             @Override
             public void actionPerformed(ActionEvent e) {
                 String name = (String) JOptionPane.showInputDialog(
--- front/src/main/java/SocketClient.java
+++ front/src/main/java/SocketClient.java
@@ -9,44 +9,50 @@ import java.awt.event.ActionListener;
 import java.net.URI;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
+import javax.swing.*;
 
 public class SocketClient {
 
-    static URI uri = URI.create("http://localhost:3000");
+    static URI uri = URI.create("http://localhost:3000");//建立URI為LocalHost,port:3000
     static IO.Options options = IO.Options.builder()
             .setQuery("x=42")
             .build();
     static Socket socket = IO.socket(uri, options);
-    static ClientUI ui = new ClientUI();
+    //建立socket
+    static ClientUI ui = new ClientUI();//使用ClientUI定義好的視窗
 
     public static void main(String[] args) {
-        ui.sendMessage.addActionListener(new ActionListener() {
+        ui.sendMessage.addActionListener(new ActionListener() { 
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (ui.inputMessage.getText() == null) return;
-                JSONObject messageWillSend = new JSONObject();
-                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-                LocalDateTime now = LocalDateTime.now();
-                try {
-                    messageWillSend.put("username", ui.NAME);
-                    messageWillSend.put("message", ui.inputMessage.getText());
-                    messageWillSend.put("time", dtf.format(now));
-                    ui.setFromServer(messageWillSend);
-                } catch (JSONException jsonException) {
-                    jsonException.printStackTrace();
+                if (ui.inputMessage.getText().length() == 0){ //使用沒有輸入時，產生錯誤對話框
+                    JOptionPane.showMessageDialog(null,"請輸入文字","error",JOptionPane.ERROR_MESSAGE); 
                 }
-                socket.emit("sendMessage", messageWillSend.toString());
-                ui.inputMessage.setText("");
+                else if (ui.inputMessage.getText().length() != 0){  //使用者有輸入時
+                    JSONObject messageWillSend = new JSONObject();  //建立一個JSON物件來傳遞資料
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");//產生日期字串
+                    LocalDateTime now = LocalDateTime.now();//擷取目前時間以及日期（不考慮時區）
+                    try {
+                        messageWillSend.put("username", ui.NAME);//取得輸入的使用者名稱
+                        messageWillSend.put("message", ui.inputMessage.getText());//取得ClientUI使用者輸入的訊息
+                        messageWillSend.put("time", dtf.format(now));//取得輸入時間
+                        ui.setFromServer(messageWillSend);//呼叫setFromServer更新messages（JTextPane）內容
+                    } catch (JSONException jsonException) {
+                        jsonException.printStackTrace();
+                    }
+                    socket.emit("sendMessage", messageWillSend.toString());//對當前連線的client發送一個"sendMessage"事件
+                    ui.inputMessage.setText("");//傳送完後將JTextField舊的內容清空
+                }
             }
         });
-        socket.on("getMessage", new Emitter.Listener() {
+        socket.on("getMessage", new Emitter.Listener() { //監聽來自server的"getMessage"事件
             @Override
-            public void call(Object... args) { // args[0] here will be the message from other clients.
+            public void call(Object... args) { // args[0]是來自其他Client端的訊息
                 try {
-                    JSONObject message = new JSONObject(args[0].toString());
+                    JSONObject message = new JSONObject(args[0].toString());//建立一個JSON物件來傳遞資料
                     System.out.println(message);
                     System.out.println(args[0]);
-                    ui.setFromServer(message);
+                    ui.setFromServer(message);//呼叫setFromServer更新messages（JTextPane）內容
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
