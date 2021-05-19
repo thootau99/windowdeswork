d2e81423abe1cfa91925d77715f47a86b36f6901
Thootau <thootau99@tutanota.com>
2021-05-19 11:17:00 +0800

    Tired

--- front/src/main/java/SocketClient.java
+++ front/src/main/java/SocketClient.java
@@ -16,7 +16,7 @@ import javax.swing.event.MouseInputAdapter;
 
 public class SocketClient {
 
-    static URI uri = URI.create("http://thootau.synology.me:48763");//建立URI為LocalHost,port:3000
+    static URI uri = URI.create("http://thootau.synology.me:48763");//
     static IO.Options options = IO.Options.builder()
             .build();
     static Socket socket = IO.socket(uri, options);
